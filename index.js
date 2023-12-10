318af20896c2975c1f5bc494f817bc73f5a8dd18
Reyyen <133241336+Reyyen@users.noreply.github.com>
2023-12-10 11:01:06 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-
-document.querySelector(".btn-ekle").addEventListener("click",function (urunSayisi) {
-    
-    document.querySelector(".num-of-urun").textContent =+document.querySelector(".num-of-urun").innerHTML+1 ;
-    
-})
-
-
-const heartCheckboxes = document.querySelectorAll(".heart-checkbox");
-const heartLabels = document.querySelectorAll(".heart-label");
-
-heartCheckboxes.forEach(function (checkbox, index) {
-    checkbox.addEventListener("change", function () {
-    if (this.checked) {
-        heartLabels[index].classList.add("checked");
-        document.querySelector(".num-of-fa").textContent =+document.querySelector(".num-of-fa").innerHTML+1 ;
-
-    } else {
-        heartLabels[index].classList.remove("checked");
-        document.querySelector(".num-of-fa").textContent =+document.querySelector(".num-of-fa").innerHTML-1 ;
-    }
-    });
-});
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+var i = document.querySelectorAll(".drum").length;
+for (let index = 0; index < i; index++) {
+    document.querySelectorAll(".drum")[index].addEventListener("click", function (){
+        var btninnerHtml = this.innerHTML;
+        sound(btninnerHtml);
+        butAnimation(btninnerHtml);
+    }); 
 }
 
-window.addEventListener("scroll", function () {
-    var button = document.getElementById("scrollToTopButton");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        button.style.display = "block"; // Show the button when scrolled down
-    } else {
-        button.style.display = "none"; // Hide the button when at the top
-        
-    }
+document.addEventListener("keypress",function (event) {
+    sound(event.key);
+    butAnimation(event.key);
 });
 
-function closePopup() {
-    var popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-}
+/*var audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();*/
+    
+    function sound(key) {
+        switch(key){
+            
+            case "w":
+                var s1 = new Audio("./sounds/tom-1.mp3");
+                s1.play();
+                break;
+            case"a":
+                var s2=new Audio ("./sounds/tom-2.mp3") ;
+                s2.play ();
+                break;
+            case's':
+                var s3=new Audio ('./sounds/tom-3.mp3') ;
+                s3.play () ;
+                break;
+            case"d":
+                var s4=new Audio('./sounds/tom-4.mp3');
+                s4.play() ;
+                break;
+            case 'j':
+                var s5=new Audio('./sounds/snare.mp3');
+                s5.play() ;
+                break;
+            case"k":
+                var s6=new Audio("./sounds/crash.mp3");
+                s6.play() ;
+                break;
+            case"l":
+                var s7=new Audio('./sounds/kick-bass.mp3');
+                s7.play() ;
+                break;
+        }
+    }
+
+    function butAnimation(currentKey) {
+        var activebtn = document.querySelector("."+currentKey);
+        activebtn.classList.add("pressed");
+        setTimeout(function (){
+            activebtn.classList.remove('pressed')} ,100 );
+    }
